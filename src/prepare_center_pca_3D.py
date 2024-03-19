ed2ed1f6e7fe359a55700aeffcedc1b4fe1561d4
robgirod <71637217+robgirod@users.noreply.github.com>
2024-03-19 20:45:01 +0100

    Update_PCA

--- src/prepare_center_pca_3D.py
+++ src/prepare_center_pca_3D.py
@@ -7,13 +7,14 @@ align axes with PCA
 """
 
 from skimage import registration
-from skimage.transform import warp, downscale_local_mean, EuclideanTransform, rotate
+from skimage.transform import warp, downscale_local_mean, EuclideanTransform, affine_transform
 from skimage import img_as_float32
 from skimage.exposure import rescale_intensity
 import mrcfile
 from skimage.metrics import hausdorff_distance, hausdorff_pair
 from skimage.filters import threshold_otsu, sobel
 from skimage.draw import line_nd, ellipse, polygon
+from scipy.ndimage import affine_transform
 import numpy as np
 from sklearn import decomposition
 
@@ -39,42 +40,43 @@ def translate_center_of_mass(img):
 
 def rotate_pca(img):
 
+    # Reduce size if needed, here if any axis is > 150 voxels
+    if np.any(np.array(img.shape) > 150):
+        bin_factor = np.ceil(max(img.shape) / 150)
+        img = downscale_local_mean(img, bin_factor)
+
+        print(f'Reduced the image size to {img.shape} for speed')
+
     # Find indices of the data points
     edges = sobel(img)
     otsu = threshold_otsu(edges)
     edges = edges > otsu
-    y, x = np.nonzero(edges)
+    z, y, x = np.nonzero(edges)
 
     # Center at center of mass
     x_centered = x - x.mean()
     y_centered = y - y.mean()
-    YX = np.vstack((y_centered, x_centered)).T
+    z_centered = z - z.mean()
+
+    ZYX = np.vstack((z_centered, y_centered, x_centered)).T
 
     # Compute PCA to get axes of inertia
-    pca = decomposition.PCA(n_components = 2)
-    pca.fit(YX)
+    pca = decomposition.PCA(n_components = 3)
+    pca.fit(ZYX)
     rot = pca.components_ # Retrieves the rotation matrix
 
-    # Flip Rotation matrix PC1 and PC2 if det is -1
-    # if round(np.linalg.det(rot)) == -1:
-    #   rot = np.flipud(rot)
+    slices, rows, cols = img.shape[0], img.shape[1], img.shape[2] 
+    center = np.array((slices, cols, rows)) / 2. - 0.5
 
-
-    # Create transformation matrix
-    # First, center the rotation
-    # to ensure that the image is rotated around the center of the image/volume
-    rows, cols = img.shape[0], img.shape[1]
-    center = np.array((cols, rows)) / 2. - 0.5
-
-    tform1 = EuclideanTransform(translation = center)
+    tform1 = EuclideanTransform(translation = center, dimensionality=3)
 
     # Rotation
-    tform2 = EuclideanTransform(rotation = 0)
-    tform2.params[:2, :2] = rot
+    tform2 = EuclideanTransform(rotation = (0, 0, 0), dimensionality=3)
+    tform2.params[:3, :3] = rot.T
 
     # Back to original position
-    tform3 = EuclideanTransform(translation = -center)
+    tform3 = EuclideanTransform(translation = -center, dimensionality=3)
 
     # Perform the transforms
     tform = tform3 + tform2 + tform1
-    return warp(img, tform, order = 0, mode = 'constant'), rot
+    return affine_transform(img, tform, order = 0, mode = 'constant'), rot.T
