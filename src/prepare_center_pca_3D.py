99a276ee537d7dda9899b8570a6b5422ba6d8730
robgirod <robin.girod@proton.me>
2024-03-28 14:26:31 +0100

    Add axis alignment and rod_like simplification of grid search

--- src/prepare_center_pca_3D.py
+++ src/prepare_center_pca_3D.py
@@ -16,6 +16,8 @@ from skimage.filters import threshold_otsu, sobel
 from skimage.draw import line_nd, ellipse, polygon
 import numpy as np
 from sklearn import decomposition
+from scipy.ndimage import affine_transform
+
 
 # ----------------- BEGIN CODE ------------------
 
@@ -27,15 +29,17 @@ def translate_center_of_mass(img):
     edges = edges > otsu
 
     # Center of image
-    center = img.shape//2
+    center = (img.shape[0]/2, img.shape[1]/2, img.shape[2]/2) 
 
     # Center of particle
-    z, y, x = np.nonzero(edges)
-    center_x = x.mean() - center[0]
-    center_y = y.mean() - center[0]
-    tform = EuclideanTransform(translation = (center_x, center_y))
-    
-    return warp(img, tform, order = 0, mode = 'wrap')
+    z, y, x = np.nonzero(img)
+    center_x = x.mean() - center[2]
+    center_y = y.mean() - center[1]
+    center_z = z.mean() - center[0]
+
+    tform = EuclideanTransform(translation = (center_z, center_y, center_x), dimensionality=3)
+
+    return affine_transform(img, tform, order = 0, mode = 'constant')
 
 def rotate_pca(img):
 
@@ -45,38 +49,36 @@ def rotate_pca(img):
     edges = edges > otsu
     z, y, x = np.nonzero(edges)
 
-    # Center at center of mass
+    # Center at (binary) center of mass
     x_centered = x - x.mean()
     y_centered = y - y.mean()
     z_centered = z - z.mean()
 
     ZYX = np.vstack((z_centered, y_centered, x_centered)).T
 
-    # Compute PCA to get axes of inertia
-    pca = decomposition.PCA(n_components = 2)
-    pca.fit(YX)
-    rot = pca.components_ # Retrieves the rotation matrix
+    # Compute PCA to get major axes of the particle
+    pca = decomposition.PCA(n_components = 3)
+    pca.fit(ZYX)
+    rot = pca.components_ # Retrieves the rotation matrix, this will work for the major axis on 0
 
-    # Flip Rotation matrix PC1 and PC2 if det is -1
-    # if round(np.linalg.det(rot)) == -1:
-    #   rot = np.flipud(rot)
+    center = np.array(img.shape) // 2
 
-
-    # Create transformation matrix
-    # First, center the rotation
-    # to ensure that the image is rotated around the center of the image/volume
-    slices, rows, cols = img.shape[0], img.shape[1]
-    center = np.array((slices, cols, rows)) / 2. - 0.5
-
-    tform1 = EuclideanTransform(translation = center)
+    # Ensures rotation is centered
+    tform1 = EuclideanTransform(translation = -center, dimensionality=3)
 
     # Rotation
-    tform2 = EuclideanTransform(rotation = 0)
-    tform2.params[:2, :2] = rot
+    tform2 = EuclideanTransform(rotation = (0, 0, 0), dimensionality=3)
+    tform2.params[:3, :3] = rot.T
 
-    # Back to original position
-    tform3 = EuclideanTransform(translation = -center)
+    # Back to original coordinates
+    tform3 = EuclideanTransform(translation = center, dimensionality=3)
 
-    # Perform the transforms
-    tform = tform3 + tform2 + tform1
-    return warp(img, tform, order = 0, mode = 'constant'), rot
+    tform = tform1 + tform2 + tform3
+
+    Q_aligned = affine_transform(img, tform, order = 0, mode = 'constant')
+
+    # The rotation sometimes flips an axis, esp with det(rot) != 1, which is weird ...
+    if np.linalg.det(rot.T) < 0:
+        return np.flip(Q_aligned, axis = 2)
+    else:
+        return Q_aligned
